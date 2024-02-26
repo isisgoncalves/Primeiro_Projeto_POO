62a3dff07c0e2126d719f61e624b5d143204a761
isisgoncalves <isisvow@gmail.com>
2024-02-26 19:02:40 -0300

    Create Pessoa.cs

---
+++ Models/Pessoa.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Primeiro_Projeto_POO.Models
+{
+    public class Pessoa
+    {
+        
+    }
+}
