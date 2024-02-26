50af93f2eb006d129445c28cff66fd1018d632fb
isisgoncalves <isisvow@gmail.com>
2024-02-26 19:16:41 -0300

    Update Pessoa.cs

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -1,12 +1,23 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace Primeiro_Projeto_POO.Models
+class Pessoa
 {
-    public class Pessoa
+    private string nome;
+    private int idade;
+
+    public Pessoa(string n, int i)
     {
-        
+        nome = n;
+        idade = i;
+    }
+
+    public string GetNome()
+    {
+        return nome;
+    }
+
+    public int GetIdade()
+    {
+        return idade;
     }
 }
